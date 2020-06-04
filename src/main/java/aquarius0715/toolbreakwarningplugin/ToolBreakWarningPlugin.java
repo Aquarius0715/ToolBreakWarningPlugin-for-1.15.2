4b2b6301aaa4d49eae4b493bbd7d01db3e7f5f58
BuildTools <unconfigured@null.spigotmc.org>
2020-06-04 12:45:16 +0900

    Removed title, Add stopper, version 1.1.0

--- src/main/java/aquarius0715/toolbreakwarningplugin/ToolBreakWarningPlugin.java
+++ src/main/java/aquarius0715/toolbreakwarningplugin/ToolBreakWarningPlugin.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -23,17 +24,23 @@ import java.util.UUID;
 public final class ToolBreakWarningPlugin extends JavaPlugin implements Listener {
 
     Map<UUID, Boolean> settings = new HashMap<UUID, Boolean>();
+    Map<UUID, Boolean> stopper_stats = new HashMap<UUID, Boolean>();
     String prefix = ChatColor.BOLD + "[" + ChatColor.GREEN + "ToolBreakWarning" + ChatColor.WHITE + "" + ChatColor.BOLD + "] ";
+    boolean plugin_stats = false;
 
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
         Objects.requireNonNull(getCommand("tbw")).setExecutor(this);
+        plugin_stats = true;
         // Plugin startup logic
     }
 
     @Override
     public void onDisable() {
+        settings.clear();
+        stopper_stats.clear();
+        plugin_stats = false;
         // Plugin shutdown logic
     }
 
@@ -51,8 +58,11 @@ public final class ToolBreakWarningPlugin extends JavaPlugin implements Listener
                 sender.sendMessage(prefix + "壊れそうになると音で知らせてくれるプラグインです。");
                 sender.sendMessage(prefix + "デフォルトではonになっています。");
                 sender.sendMessage(prefix + "</tbw>: この説明画面を開きます。");
+                sender.sendMessage(prefix + "</tb stopper>: ストッパー機能の有効・無効を指定します。");
                 sender.sendMessage(prefix + "</tbw on>: このプラグインを使用します。");
                 sender.sendMessage(prefix + "</tbw off>: このプラグインの使用をやめます。");
+                sender.sendMessage(prefix + "</tbw enable>: このプラグインを有効化にします。");
+                sender.sendMessage(prefix + "</tbw disable>: このプラグインを無効化します。");
                 sender.sendMessage(prefix + "===============ToolBreakWarningPlugin===============");
             }
 
@@ -60,22 +70,65 @@ public final class ToolBreakWarningPlugin extends JavaPlugin implements Listener
                 Player player = (Player) sender;
 
                 if (args[0].equalsIgnoreCase("on")) {
+                    if (!plugin_stats) {
+                        sender.sendMessage(prefix + "プラグインは停止しています。");
+                        return false;
+                    }
                     settings.put(player.getUniqueId(), true);
                     player.sendMessage(prefix + ChatColor.GREEN + "" + ChatColor.BOLD + "通知機能をオンにしました。");
-                } else if (args[0].equalsIgnoreCase("off")) {
+                }
+                if (args[0].equalsIgnoreCase("off")) {
+                    if (!plugin_stats) {
+                        sender.sendMessage(prefix + "プラグインは停止しています。");
+                        return false;
+                    }
                     settings.put(player.getUniqueId(), false);
                     player.sendMessage(prefix + ChatColor.RED + "" + ChatColor.BOLD + "通知機能をオフにしました。");
-                } else {
-                    sender.sendMessage(prefix + "===============ToolBreakWarningPlugin===============");
-                    sender.sendMessage(prefix + "このプラグインはツールの耐久値があといくつなのか、");
-                    sender.sendMessage(prefix + "壊れそうになると音で知らせてくれるプラグインです。");
-                    sender.sendMessage(prefix + "デフォルトではonになっています。");
-                    sender.sendMessage(prefix + "</tbw>: この説明画面を開きます。");
-                    sender.sendMessage(prefix + "</tbw on>: このプラグインを使用します。");
-                    sender.sendMessage(prefix + "</tbw off>: このプラグインの使用をやめます。");
-                    sender.sendMessage(prefix + "===============ToolBreakWarningPlugin===============");
+                }
+                if (args[0].equalsIgnoreCase("disable")) {
+                    if (!player.hasPermission("admin")) {
+                        sender.sendMessage(prefix + "あなたはこのコマンドを使うことができません。");
+                        return false;
+                    }
+                    if (!plugin_stats) {
+                        sender.sendMessage(prefix + "プラグインはすでに無効にされています。");
+                        return false;
+                    } else {
+                        onDisable();
+                        sender.sendMessage(prefix + "プラグインを無効にしました。");
+                        return true;
+                    }
+                }
+                if (args[0].equalsIgnoreCase("enable")) {
+                    if (plugin_stats && !player.hasPermission("admin")) {
+                        sender.sendMessage(prefix + "あなたはこのコマンドを使うことができません。");
+                        return false;
+                    }
+                    if (plugin_stats) {
+                        sender.sendMessage(prefix + "プラグインはすでに有効にされています。");
+                        return false;
+                    }
+                }
+                if (!plugin_stats) {
+                    onEnable();
+                    sender.sendMessage(prefix + "プラグインを有効にしました。");
+                    return true;
+                }
+                if (args[0].equalsIgnoreCase("stopper")) {
+                    if (!stopper_stats.containsKey(player.getUniqueId())) {
+                        stopper_stats.put(((Player) sender).getUniqueId(), true);
+                        sender.sendMessage(prefix + "ストッパー機能をオンにしました。");
+                        return true;
+                    }
+
+                    if (stopper_stats.containsKey(player.getUniqueId())) {
+                        stopper_stats.put(((Player) sender).getUniqueId(), false);
+                        sender.sendMessage(prefix + "ストッパー機能をオフにしました。");
+                        return true;
+                    }
                 }
             }
+            return false;
         }
         return false;
     }
@@ -88,6 +141,10 @@ public final class ToolBreakWarningPlugin extends JavaPlugin implements Listener
         int nowDurability = (event.getItem().getType().getMaxDurability() - event.getItem().getDurability()) - 1;
 
         boolean contains = settings.containsKey(player.getUniqueId());
+        if (!plugin_stats) {
+            event.isCancelled();
+            return;
+        }
         if (!contains) {
             settings.put(player.getUniqueId(), true);
         }
@@ -113,11 +170,35 @@ public final class ToolBreakWarningPlugin extends JavaPlugin implements Listener
         }
     }
 
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+
+        boolean contains = stopper_stats.containsKey(player.getUniqueId());
+
+        if (!plugin_stats || !contains) {
+            event.isCancelled();
+            return;
+        }
+
+        if (stopper_stats.get(player.getUniqueId()).equals(true)) {
+            int nowDurability = (player.getItemInHand().getType().getMaxDurability() - player.getItemInHand().getDurability()) - 1;
+            String message = ChatColor.RED + "" + ChatColor.BOLD + "ストッパーが作動しました！！！";
+            TextComponent component = new TextComponent();
+            component.setText(message);
+
+            if (nowDurability == 0) {
+                event.setCancelled(true);
+                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, component);
+            }
+        }
+    }
+
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-
-        player.sendMessage(prefix + "ToolBreakWarning は有効化されています。詳細は「/tbw」と入力して確認して下さい。");
-
+        if (plugin_stats) {
+            player.sendMessage(prefix + "ToolBreakWarning は有効化されています。詳細は「/tbw」と入力して確認して下さい。");
+        }
     }
 }
